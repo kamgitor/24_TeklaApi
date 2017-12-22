039cf737ece87bb8ac0756f87c5c2b48f33c6165
Kamil <kamil@altel.pl>
2017-12-22 14:24:52 +0100

    First commit

---
+++ TeklaTest/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using TSM = Tekla.Structures.Model;
+using TSG3D = Tekla.Structures.Geometry3d;
+using TSD = Tekla.Structures.Drawing;
+using TS = Tekla.Structures;
+using Tekla.Structures.Model.Operations;
+
+/*
+	links
+	http://www.tek1.com.au/set-tekla-api-project-tekla-api-application/
+
+ 
+ */
+
+
+
+
+namespace TeklaTest
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+
+	}
+}
